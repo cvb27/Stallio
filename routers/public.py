49a0f5da998817fc1a42a59ae0c5e9799a0211e0
cvb27 <carlosvblancom@gmail.com>
2025-09-25 08:18:06 -0400

    requirements  update

--- routers/public.py
+++ routers/public.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Request, Form, Depends, WebSocket, WebSocketDisconnect, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse, JSONResponse
-from starlette.responses import EventSourceResponse
+from sse_starlette.sse import EventSourceResponse
 from sqlmodel import Session, select
 from models import Product, PaymentReport, User
 from db import get_session
